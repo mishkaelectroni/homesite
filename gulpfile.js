8702c3b0c883bcffbfd237c6a0c6d95d172fe2bb
mishkaelectroni <mishkaelectroni@gmail.com>
2017-10-19 16:17:27 +0300

    19102017homesitehtml
    19102017homesitehtml

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var plumber=require('gulp-plumber');
+var watch=require('gulp-watch');
+gulp.task('sass', function(){
+	gulp.src('scss/main.scss')
+	.pipe(plumber())
+	.pipe(sass({errLogToConsole:true}))
+	.pipe(gulp.dest('project/css'));
+});
+gulp.task('default',function(){
+	gulp.run('sass');
+	gulp.watch('scss/**',function(event){
+		gulp.run('sass');
+	})
+})
